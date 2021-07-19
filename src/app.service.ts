469d45bb763de9418b746b8679335e0414a94161
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-07-19 19:22:46 -0300

    Pega as estatísticas do usuário

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,12 +6,8 @@ import fetch from 'node-fetch';
 export class AppService {
   // eslint-disable-next-line prettier/prettier
   async getGithubProfileMetrics({ user }: IGithubRepository): Promise<IGithubRepository> {
-    return fetch(`https://api.github.com/users/${user}`)
-      .then((res) => {
-        return res.json();
-      })
-      .catch((error) => {
-        throw error.json();
-      });
+    return fetch(`https://api.github.com/users/${user}/starred`).then((res) => {
+      return res.json();
+    });
   }
 }
