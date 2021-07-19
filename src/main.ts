216e5db2f2996cbe5b18fffb4bbbec0c640c98d7
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-07-19 20:03:50 -0300

    Adição do serverless

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverless from 'serverless-http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
+
+module.exports.hander = serverless(bootstrap);
