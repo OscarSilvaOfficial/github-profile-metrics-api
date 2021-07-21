e3a7bba3500330794cb4b27b03ee415744185f4d
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-07-20 22:04:23 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverless from 'serverless-http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,12 +7,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-
-exports.handler = (event, context, callback) => {
-  if (event.httpMethod === 'GET' && event.path === '/') {
-    callback(null, {
-      statusCode: 200,
-      body: 'Rwarw',
-    });
-  }
-};
