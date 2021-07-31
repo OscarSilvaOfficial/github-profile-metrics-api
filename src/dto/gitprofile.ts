3fdfa935801eb3f4d3900623c252b96ecf1f3aa6
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-07-31 00:58:48 -0300

    Retorno apenas dos dados necessários

--- src/dto/gitprofile.ts
+++ src/dto/gitprofile.ts
@@ -6,6 +6,22 @@ export class GitProfileDTO {
   }
 
   get getData() {
-    return this.user;
+    return this.user.map((data) => {
+      return {
+        public: data['public'],
+        created_at: data['created_at'],
+        type: data['type'],
+        actor: {
+          login: data['actor']['login'],
+          url: data['actor']['url'],
+          avatar: data['actor']['avatar_url'],
+        },
+        repo: {
+          name: data['repo']['name'],
+          url: data['repo']['url'],
+        },
+        commits: data['payload']['commits'],
+      };
+    });
   }
 }
